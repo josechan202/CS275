f326a341fd36dfcfa8f33481cdbf9131a844858b
Jack Hurley <jackhurley@ip0af54fa7.int.uvm.edu>
2017-10-29 15:52:35 -0400

    Resolved merge conflicts.

--- database_server_side/constants.php
+++ database_server_side/constants.php
@@ -1,11 +1,7 @@
 <?php
 // print "<p>SQL: " . $sql . "<pre>"; print_r($data); print "</pre></p>";     
 // print "<p>Array:<pre>"; print_r($results); print "</pre></p>";
-<<<<<<< HEAD
-define("DATABASE_NAME", 'ABARSON' . '_TEST');
-=======
 define("DATABASE_NAME", "ABARSON" . '_TEST');
->>>>>>> origin/master
 define("ADMIN_EMAIL", get_current_user() . "@uvm.edu");
 define("LINE_BREAK", "\n");
 ?>
