e6d3b719a631c30f2e55be97dfef435c20a11e58
Jack Hurley <jfhurley@uvm.edu>
2017-11-29 22:12:15 -0500

    Began making the UI elements for the post page (aka repurposing the homepage). Work is not functional yet.

---
+++ Clubster/Clubster/PostCell.swift
@@ -0,0 +1,16 @@
+//
+//  PostCell.swift
+//  Clubster
+//
+//  Created by Jack Frederick Hurley on 11/29/17.
+//  Copyright © 2017 Adam Barson. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UICollectionViewCell {
+    
+    @IBOutlet weak var toClub: UIButton!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var postBody: UITextView!
+}
