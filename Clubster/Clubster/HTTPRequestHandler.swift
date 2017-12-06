58954d5f0c67daea71db6b5ca9b240b74fd420f0
Jack Hurley <jfhurley@uvm.edu>
2017-12-05 21:34:57 -0500

    working on adding posts. the python script isn't working.

--- Clubster/Clubster/HTTPRequestHandler.swift
+++ Clubster/Clubster/HTTPRequestHandler.swift
@@ -133,6 +133,60 @@ public class HTTPRequestHandler {
     }
     
     
+    public class func addPost(username : String, clubname: String, messageString: String,
+                                successHandler: @escaping (_ success: Bool, _ message: String?) -> Void)->Void {
+        let url = URL(string: "https://\(Constants.ZOO_NAME).w3.uvm.edu/add_post.py")!
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        
+        let parameterDictionary = ["username" : username, "clubname" : clubname, "message" : messageString]
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameterDictionary, options: []) else {
+            return
+        }
+        print("request = \(parameterDictionary)")
+        
+        request.httpBody = httpBody
+        //print("request = \(request.httpBody)\n")
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("error=\(error!)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response!)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            
+            print("json response     = \(responseString!)")
+            
+            do {
+                // Convert the data to JSON
+                let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                
+                if let json = jsonSerialized, json["success"] as! Bool{
+                    print("Successfully Changed \(username)'s post for status to club \(clubname) ")
+                    
+                    successHandler(true, json["message"] as! String)
+                } else {
+                    successHandler(false, "not serialized")
+                    
+                }
+            }  catch let error as NSError {
+                successHandler(false, error as! String)
+            }
+            
+            
+            
+        }
+        
+        task.resume()
+    }
+    
     public class func makeGetRequest(successHandler: @escaping (_ response: String) -> Void)->Void {
         let url = URL(string: "https://www.uvm.edu/~abarson/rest/example.php?net_id=abarson")
         
--- python_serverside/add_post.py
+++ python_serverside/add_post.py
@@ -20,38 +20,50 @@ def application(environ, start_response):
             request_body = environ['wsgi.input'].read(request_body_size)
             j = json.loads(request_body)
             username = j['username']
-            password = j['password']
             clubname = j['clubname']
             message = j['message']
         except:
             start_response("400 argument error", [('Content_Type','application/json')])
-            return json.dumps({"success": False, "message": "parameters missing"})
+            return json.dumps({"success": False, "message": "parameters missing j = " + j})
 
 
 
         cursor = cnx.cursor()
         
-        rows_count = cursor.execute("SELECT username FROM User WHERE username = %s AND password = %s;", (username, password,))
+        rows_count = cursor.execute("SELECT username FROM User WHERE username = %s;", (username))
 
         if rows_count == 0:
-            start_response("201 OK", [('Content_Type','application/json')])
+            start_response("404 Not Found", [('Content_Type','application/json')])
             return json.dumps({"success": False, "message": "Username or password invalid"})
+                
+                
         
+        sql = "SELECT Admins.club_id FROM Admins INNER JOIN Club ON Admins.club_id=Club.club_id WHERE Admins.username = %s AND Club.clubname = %s"
         
-        rows_count = cursor.execute("SELECT club_id FROM Subscriptions WHERE username = %s;", (username,))
+        rows_count = cursor.execute(sql, (username, clubname))
         
         if rows_count == 0:
-            start_response("201 success", [('Content_Type','application/json')])
-            return json.dumps({"success": True, "username": username, "clubs": []})
+            start_response("401 Unauthorized", [('Content_Type','application/json')])
+            return json.dumps({"success": False, "message": "User " + username + "Does not have admin privilages over club " + clubname})
         else:
-            start_response("201 success", [('Content_Type','application/json')])
             
-            club_list = cursor.fetchall()
-            clubs_out = []
-            for i in range(rows_count):
-                clubs_out.append(str(club_list[i][0]))
+            club_id = cursor.fetchall()
+            
+            sql = "INSERT INTO Notification (club_id, post_body) VALUES (%s, %s)"
+            
+            try:
+                cursor.execute(sql, (club_id, message))
+                cnx.commit()
+                success = True
+                message = "Message successfully added."
+                start_response("201 success", [('Content_Type','application/json')])
+            
+            except:
+                success = False
+                message = "Database failed to add notification."
+                start_response("500 Internal Server Error", [('Content_Type','application/json')])
 
-            return json.dumps({"success": True, "username": username, "clubs": clubs_out})
+            return json.dumps({"success": success, "message": message})
     
 
     start_response("400 error",[('Content-Type','text/html')])
